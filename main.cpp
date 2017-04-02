6ad5a0dabf73b91437d7845e96fc72611ddecfa1
洪念綺 <jennyhung1212@gmail.com>
2017-04-02 11:29:57 +0800

    bug fixed

--- main.cpp
+++ main.cpp
@@ -17,15 +17,34 @@ void original_gene(int** parent_pool, int park_num){
         for(int j=0; j< park_num; j++){
             if(rand()%6 == 0){
                 parent_pool[i][j] = 0;
-            }else if(rand()%5 == 1){
+            }else if(rand()%6 == 1){
                 parent_pool[i][j] = 1;
-            }else if(rand()%5 == 2){
+            }else if(rand()%6 == 2){
                 parent_pool[i][j] = 2;
-            }else if(rand()%5 == 3){
+            }else if(rand()%6 == 3){
                 parent_pool[i][j] = 3;
-            }else if(rand()%5 == 4){
+            }else if(rand()%6 == 4){
                 parent_pool[i][j] = 4;
+            }else if(rand()%6 == 5){
+                parent_pool[i][j] = 5;
             }
         }
     }
+    for(int i=0; i<25; i++){
+        for(int j=0; j<park_num; j++){
+            cout<<parent_pool[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
+
+int main(){
+    int park_num;
+    cin>>park_num;
+    int** parent_pool= new *int[park_num];
+    for(int i=0; i<10; i++){
+        parent_pool[i] = {0};
+    }
+    original_gene();
+}
+
