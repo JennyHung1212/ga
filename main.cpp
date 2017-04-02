4ac3e8b1c52c7348ea848f8e347efef93ef31bf8
洪念綺 <jennyhung1212@gmail.com>
2017-04-02 11:14:17 +0800

    [function added] original gene added

--- main.cpp
+++ main.cpp
@@ -8,4 +8,24 @@
 #include <string>
 
 using namespace std;
-12323
+
+
+
+void original_gene(int** parent_pool, int park_num){
+    srand(unsigned(time(NULL)));
+    for(int i=0; i<25; i++){
+        for(int j=0; j< park_num; j++){
+            if(rand()%6 == 0){
+                parent_pool[i][j] = 0;
+            }else if(rand()%5 == 1){
+                parent_pool[i][j] = 1;
+            }else if(rand()%5 == 2){
+                parent_pool[i][j] = 2;
+            }else if(rand()%5 == 3){
+                parent_pool[i][j] = 3;
+            }else if(rand()%5 == 4){
+                parent_pool[i][j] = 4;
+            }
+        }
+    }
+}
